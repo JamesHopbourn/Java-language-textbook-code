9a1d9db211435f38f20bd59ae9a96ebb8bd11b49
JamesHopbourn <jameshopbourn@gmail.com>
2021-09-21 01:42:35 +0800

    E212.java
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@ clang-format -i -style=LLVM *.java
 rm -rf ./build/*
 mkdir -p ../build
 javac $(find . -name "*.java") -d build
+
+if [[ $1 != '0' ]]; then
+  echo -e "\n\033[32mExecute result\033[0m"
+  java -cp $(pwd)/build/ $(basename $1 .class)
+  exit 0
+fi
