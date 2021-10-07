080c80febda7010a789a1416f1c00a19c68c3788
JamesHopbourn <jameshopbourn@gmail.com>
2021-10-07 23:50:52 +0800

    Experiment04
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- 211007 Experiment/Experiment04.java
+++ 211007 Experiment/Experiment04.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 public class Experiment04 {
   public static void main(String[] args) {
     System.out.println("班级 名字 学号");
-    System.out.print("请输入 n 的值：");
+    System.out.print("请输入 x 的值：");
     Scanner text = new Scanner(System.in);
-    int n = text.nextInt();
-    int count = 0;
-    for (int i = 2; i < n; i++) {
-      for (int j = 2; j <= i; j++) {
-        if (i == j) {
-            System.out.print(i + "\t");
-            count++;
-            if (count %5 == 0)
-              System.out.println();
-        } else if (i % j == 0) {
-            break;
-        }
+    int n = text.nextInt(), count = 0;
+
+    for (int i = 2 ; i < n ; i++) {
+      if (isPrime(i)){
+        System.out.print(i + "\t");
+        count++;
+        if (count % 5 == 0) System.out.println();
       }
     }
   }
-}
 
+  public static boolean isPrime(int N) {
+    if (N < 2) return false;
+    for (int i  = 2 ; i <= Math.sqrt(N) ; i++)
+      if (N % i == 0) return false;
+    return true;
+  }
+}
