04f199d419b560c52c74f3eaf4e52e8429c2eec0
JamesHopbourn <jameshopbourn@gmail.com>
2021-11-18 22:15:45 +0800

    interface
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

---
+++ interface/Example_0312.java
@@ -0,0 +1,20 @@
+class Circle implements Shape{
+  double PI = 3.14;
+  double radius;
+  public Circle(double radius){
+    this.radius = radius;
+  }
+  public double getArea(){
+    return PI * radius * radius;
+  }
+  public void info(){
+    System.out.print(getArea());
+  }
+}
+
+public class Example_0312{
+  public static void main(String[] args) {
+    Circle c = new Circle(5);
+    c.info();
+  }
+}
