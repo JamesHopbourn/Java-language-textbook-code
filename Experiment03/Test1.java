e144365499fd7a16ef7d1f3b951df6d563bfef38
JamesHopbourn <jameshopbourn@gmail.com>
2021-11-24 23:06:25 +0800

    Test1.java

--- Experiment03/Test1.java
+++ Experiment03/Test1.java
@@ -2,6 +2,6 @@ public class Test1 {
   public static void main(String[] args) {
     System.out.println("胡金栋 202012900312 软工203");
     System.out.println(MathSelf.mul(3, 4));
-    System.out.println(MathSelfExe.mul(3.0, 4.0));
+    System.out.println(MathSelf.mul(3.0, 4.0));
   }
 }
